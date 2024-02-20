63446ef1a2c78cf877d354b46331a2b7809b887c
farzdaffarzin <ffarzin3@my.bcit.ca>
2024-02-20 10:35:30 -0800

    all the added stuf

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyBQxA-jUpuS7aJUuk7gAEYCmYsnYDIbLpc",
+    authDomain: "comp1800-demo-a2055.firebaseapp.com",
+    projectId: "comp1800-demo-a2055",
+    storageBucket: "comp1800-demo-a2055.appspot.com",
+    messagingSenderId: "895098239634",
+    appId: "1:895098239634:web:6cde1c5a6c886b56a7c44b"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
